f60b327a82ea86de9813782709967c8ab0919727
Marito <mariobrassescogarcia@gmail.com>
2016-03-24 17:45:32 +0100

    Added var new_todo(content), var last_item(create a li) and var last_item_todo(current last li item)

--- todo.js
+++ todo.js
@@ -12,7 +12,22 @@ window.onload = function() {
   doneButton.addEventListener('click', markAsDone, false);
 
 
+
+   
+
   function addToDoItem() {
+
+    //This is the content of the new to-do
+    var new_todo = document.getElementById("todo-input").value
+    //This is the new item list created where the content will be placed
+    var last_item = document.createElement("li")
+    //This is the current last item of the list
+    var last_item_todo = document.getElementById("lasttodo")
+
+    last_item.parentNode.insertBefore(new)
+    last_item_todo.parentNode.insertBefore(new_todo, last_item_todo.nextSibling)
+
+
     // add your code here
     // this should create a new list item in the to-do list
     // and set the text as the input from the todo-input field
